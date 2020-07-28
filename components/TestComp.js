ef79357aea09882e36a73b725fed522be1ca8ac9
Vjacheslav Borisov <slavb@ilb.ru>
2020-07-28 17:11:29 +0400

    test css modules

---
+++ components/TestComp.js
@@ -0,0 +1,6 @@
+import styles from './TestComp.module.css'
+
+
+export default function TestComp() {
+    return <div className={styles.red}>Test Component</div>
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import TestComp from "../components/TestComp";
+
+function HomePage() {
+    return <div>Welcome to Next.js!
+        <TestComp/>
+    </div>
+}
+
+export default HomePage;
